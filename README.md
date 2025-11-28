56bf5abc06683e8b7fabe9f28db860162a8e2366
Vallejos Robert Andres <91800andres@gmail.com>
2025-11-28 18:34:48 +0100

    formulaire fini

---
+++ README.md
@@ -0,0 +1,20 @@
+# Formulaire-de-contact
+
+## Description du projet
+Apprendre le PHP et créer un formulaire de contact.
+
+Plateforme de formation en ligne : Udemy (https://www.udemy.com/)
+
+Cours : Section 7 : Apprendre PHP et créer un formulaire de contact (https://www.udemy.com/course/formation-developpeur-web/)
+
+Formateur : John Taieb
+
+## Langages utilisés
+- HTML
+- CSS
+- PHP
+- JS
+
+## Outils utilisés
+- XAMPP
+- Visual Studio Code
--- index.php
+++ index.php
@@ -1,70 +1,3 @@
-<?php
-    $firstname = $name = $email = $phone = $message = "";
-    $firstnameError = $nameError = $emailError = $phoneError = $messageError = "";
-    $isSuccess = false;
-    $emailTo = "john@apprendre-a-coder.com";
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $firstname = verifyInput($_POST["firstname"]);
-        $name = verifyInput($_POST["name"]);
-        $email = verifyInput($_POST["email"]);
-        $phone = verifyInput($_POST["phone"]);
-        $message = verifyInput($_POST["message"]);
-        $isSuccess = true;
-        $emailText = "";
-
-        if(empty($firstname)){
-            $firstnameError = "Je veux connaitre ton prénom !";
-            $isSuccess = false;
-        }else{
-            $emailText .= "Firstname: $firstname\n";
-        }
-        if(empty($name)){
-            $nameError = "Et oui je veux tout savoir. Même ton nom !";
-            $isSuccess = false;
-        }else{
-            $emailText .= "Name: $name\n";
-        }
-        if(!isEmail($email)){
-            $emailError = "Tessaies de me rouler ? C'est pas un email ca !";
-            $isSuccess = false;
-        }else{
-            $emailText .= "Email: $email\n";
-        }
-        if(!isPhone($phone)){
-            $phoneError = "Que des chiffres et des espaces, stp...";
-            $isSuccess = false;
-        }else{
-            $emailText .= "Phone: $phone\n";
-        }
-        if(empty($message)){
-            $messageError = "Qu'est-ce que tu veux me dire ?";
-            $isSuccess = false;
-        }else{
-            $emailText .= "Message: $message\n";
-        }
-        if($isSuccess){
-            $headers = "From: $firstname $name <$email>\r\nReply-To: $email";
-            mail($emailTo, "Un message de votre site", $emailText, $headers);
-            $firstname = $name = $email = $phone = $message = "";
-        }
-    }
-
-    function isPhone($var){
-        return preg_match("/^[0-9 ]*$/", $var);
-    }
-
-    function isEmail($var){
-        return filter_var($var, FILTER_VALIDATE_EMAIL);
-    }
-
-    function verifyInput($var){
-        $var = trim($var);
-        $var = stripslashes($var);
-        $var = htmlspecialchars($var);
-        return $var;
-    }
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -76,6 +9,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
         <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/style.css">
+        <script src="js/script.js"></script>
     </head>
     <body>
         <div class="container">
@@ -85,36 +19,36 @@
             </div>
             <div class="row">
                 <div class="col-lg-10 col-lg-offset-1">
-                    <form id="contact-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" role="form">
+                    <form id="contact-form" method="post" action="" role="form">
                         <div class="row">
                             <div class="col-md-6">
                                 <label for="firstname">Prénom<span class="blue"> *</span></label>
-                                <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom" value="<?php echo $firstname; ?>">
-                                <p class="comments"><?php echo $firstnameError; ?> </p>
+                                <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom">
+                                <p class="comments"></p>
                             </div>
 
                             <div class="col-md-6">
                                 <label for="name">Nom<span class="blue"> *</span></label>
-                                <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom" value="<?php echo $name; ?>">
-                                <p class="comments"><?php echo $nameError; ?></p>
+                                <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom">
+                                <p class="comments"></p>
                             </div>
 
                             <div class="col-md-6">
                                 <label for="email">Email<span class="blue"> *</span></label>
-                                <input type="text" id="email" name="email" class="form-control" placeholder="Votre Email" value="<?php echo $email; ?>">
-                                <p class="comments"><?php echo $emailError; ?></p>
+                                <input type="text" id="email" name="email" class="form-control" placeholder="Votre Email">
+                                <p class="comments"></p>
                             </div>
 
                             <div class="col-md-6">
                                 <label for="phone">Téléphone</label>
-                                <input type="tel" id="phone" name="phone" class="form-control" placeholder="Votre téléphone" value="<?php echo $phone; ?>">
-                                <p class="comments"><?php echo $phoneError; ?></p>
+                                <input type="tel" id="phone" name="phone" class="form-control" placeholder="Votre téléphone">
+                                <p class="comments"></p>
                             </div>
 
                             <div class="col-md-12">
                                 <label for="message">Message<span class="blue"> *</span></label>
-                                <textarea id="message" name="message" class="form-control" aria-placeholder="Votre message"><?php echo $message; ?></textarea>
-                                <p class="comments"><?php echo $messageError; ?></p>
+                                <textarea id="message" name="message" class="form-control" aria-placeholder="Votre message"></textarea>
+                                <p class="comments"></p>
                             </div>
 
                             <div class="col-md-12">
@@ -125,8 +59,6 @@
                                 <input type="submit" class="button1" value="Envoyer">
                             </div>
                         </div>
-
-                        <p class="thank-you" style="display: <?php if($isSuccess) echo'block'; else echo 'none';?>">Votre message à bien été envoyer. Merci de m'avoir contacté :)</p>
 
                     </form>
                 </div>
